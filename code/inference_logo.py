52f9a36270fad73bfc52cffa0be04ab67bffdb00
revive234 <2324940023@qq.com>
2025-04-15 21:17:57 +0800

    update inference code

---
+++ code/inference_logo.py
@@ -0,0 +1,283 @@
+from pipelines.IMAGGarment_pipeline import IMAGGarment
+from pipelines.stage2_pipeline import Stage2
+import os
+import torch
+
+from PIL import Image
+from diffusers import UNet2DConditionModel, AutoencoderKL, DDIMScheduler
+from torchvision import transforms
+from transformers import CLIPImageProcessor
+from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
+from diffusers.image_processor import VaeImageProcessor
+from transformers import CLIPTextModel, CLIPTokenizer, CLIPVisionModelWithProjection
+from adapter.attention_processor import LogoCacheSAttnProcessor2_0, LogoRefSAttnProcessor2_0, LogoCacheCAttnProcessor2_0 , CAttnProcessor2_0,IPAttnProcessor2_0
+import argparse
+from adapter.resampler import Resampler
+
+
+def preprocess( logo ,mask ,height ,width):
+    vae_processor = VaeImageProcessor(vae_scale_factor=8) 
+    mask_processor = VaeImageProcessor(vae_scale_factor=8, do_normalize=False, do_binarize=True, do_convert_grayscale=True)
+    
+    logo = vae_processor.preprocess(logo, height, width)[0]
+    mask = mask_processor.preprocess(mask, height, width)[0]
+    
+    
+    return logo,mask
+def back_to_pic(logo,mask):
+
+    if logo.min() < 0:
+        logo = (logo + 1) / 2  
+    logo = logo.permute(1, 2, 0).numpy()
+
+    logo = (logo * 255).astype("uint8")
+    logo = Image.fromarray(logo)
+
+    mask = mask.squeeze(0).numpy()
+    mask = (mask * 255).astype("uint8")
+    mask = Image.fromarray(mask, mode="L")
+    return logo,mask
+
+
+def resize_img(input_image, max_side=640, min_side=512, size=None,
+               pad_to_max_side=False, mode=Image.BILINEAR, base_pixel_number=64):
+    w, h = input_image.size
+    ratio = min_side / min(h, w)
+    w, h = round(ratio * w), round(ratio * h)
+    ratio = max_side / max(h, w)
+    input_image = input_image.resize([round(ratio * w), round(ratio * h)], mode)
+    w_resize_new = (round(ratio * w) // base_pixel_number) * base_pixel_number
+    h_resize_new = (round(ratio * h) // base_pixel_number) * base_pixel_number
+    input_image = input_image.resize([w_resize_new, h_resize_new], mode)
+
+    return input_image
+
+
+def image_grid(imgs, rows, cols):
+    assert len(imgs) == rows * cols
+    max_w,max_h=0,0
+    for img in imgs :
+        max_w = max(max_w,img.size[0])
+        max_h = max(max_h,img.size[1])
+            
+    w, h = max_w,max_h
+    grid = Image.new("RGB", size=(cols * w, rows * h))
+    grid_w, grid_h = grid.size
+
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i % cols * w, i // cols * h))
+    return grid
+
+
+def prepare(args):
+    generator = torch.Generator(device=args.device).manual_seed(42)
+    
+    #stage_1 prepare
+    vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse").to(dtype=torch.float16, device=args.device)
+    tokenizer = CLIPTokenizer.from_pretrained("SG161222/Realistic_Vision_V4.0_noVAE", subfolder="tokenizer")
+    text_encoder = CLIPTextModel.from_pretrained("SG161222/Realistic_Vision_V4.0_noVAE", subfolder="text_encoder").to(
+        dtype=torch.float16, device=args.device)
+    unet = UNet2DConditionModel.from_pretrained("SG161222/Realistic_Vision_V4.0_noVAE", subfolder="unet").to(
+        dtype=torch.float16,device=args.device)
+    image_encoder  = CLIPVisionModelWithProjection.from_pretrained("h94/IP-Adapter",subfolder ="models/image_encoder").to(
+        dtype=torch.float16, device=args.device)
+
+    # set attention processor
+    attn_procs = {}
+    st = unet.state_dict()
+    for name in unet.attn_processors.keys():
+        cross_attention_dim = None if name.endswith("attn1.processor") else unet.config.cross_attention_dim
+        if name.startswith("mid_block"):
+            hidden_size = unet.config.block_out_channels[-1]
+        elif name.startswith("up_blocks"):
+            block_id = int(name[len("up_blocks.")])
+            hidden_size = list(reversed(unet.config.block_out_channels))[block_id]
+        elif name.startswith("down_blocks"):
+            block_id = int(name[len("down_blocks.")])
+            hidden_size = unet.config.block_out_channels[block_id]
+        if cross_attention_dim is None:
+            attn_procs[name] = LogoRefSAttnProcessor2_0(name, hidden_size)
+        else:
+            attn_procs[name] = IPAttnProcessor2_0( hidden_size=hidden_size, cross_attention_dim=cross_attention_dim)
+
+    unet.set_attn_processor(attn_procs)
+    adapter_modules = torch.nn.ModuleList(unet.attn_processors.values())
+    adapter_modules = adapter_modules.to(dtype=torch.float16, device=args.device)
+    del st
+
+    ref_unet = UNet2DConditionModel.from_pretrained("SG161222/Realistic_Vision_V4.0_noVAE", subfolder="unet").to(
+        dtype=torch.float16,
+        device=args.device)
+    attn_procs2 = {}
+    st = ref_unet.state_dict()
+    for name in ref_unet.attn_processors.keys():
+        cross_attention_dim = None if name.endswith("attn1.processor") else ref_unet.config.cross_attention_dim
+        if name.startswith("mid_block"):
+            hidden_size = ref_unet.config.block_out_channels[-1]
+        elif name.startswith("up_blocks"):
+            block_id = int(name[len("up_blocks.")])
+            hidden_size = list(reversed(ref_unet.config.block_out_channels))[block_id]
+        elif name.startswith("down_blocks"):
+            block_id = int(name[len("down_blocks.")])
+            hidden_size = ref_unet.config.block_out_channels[block_id]
+        # lora_rank = hidden_size // 2 # args.lora_rank
+        if cross_attention_dim is None:
+            attn_procs2[name] = LogoCacheSAttnProcessor2_0(name, hidden_size)
+        else:
+            attn_procs2[name] = LogoCacheCAttnProcessor2_0(name, hidden_size=hidden_size,
+                                                 cross_attention_dim=cross_attention_dim)  # .to(accelerator.device)]
+    ref_unet.set_attn_processor(attn_procs2)
+
+    del st
+    ref_unet.to(dtype=torch.float16,device=args.device)
+    # weights load
+    model_sd = torch.load(args.stage1_model_ckpt, map_location="cpu")["module"]
+
+    ref_unet_dict = {}
+    unet_dict = {}
+    adapter_modules_dict = {}
+    for k in model_sd.keys():
+        if k.startswith("ref_unet"):
+            ref_unet_dict[k.replace("ref_unet.", "")] = model_sd[k]
+        elif k.startswith("unet"):
+            unet_dict[k.replace("unet.", "")] = model_sd[k]
+        elif k.startswith("adapter_modules"):
+            adapter_modules_dict[k.replace("adapter_modules.", "")] = model_sd[k]
+        else:
+            print(k)
+
+    ref_unet.load_state_dict(ref_unet_dict)
+    adapter_modules.load_state_dict(adapter_modules_dict,strict=False)
+
+    noise_scheduler = DDIMScheduler(
+        num_train_timesteps=1000,
+        beta_start=0.00085,
+        beta_end=0.012,
+        beta_schedule="scaled_linear",
+        clip_sample=False,
+        set_alpha_to_one=False,
+        steps_offset=1,
+    )
+    
+    
+    #stage_2 prepare
+    base_model_path = "ruwnayml/stable-diffusion-inpainting"
+    attn_ckpt = args.stage2_model_ckpt
+    stage2_unet = UNet2DConditionModel.from_pretrained(base_model_path, subfolder="unet").to(device=args.device,dtype=torch.float16)
+    stage2_noise_scheduler = DDIMScheduler.from_pretrained(base_model_path, subfolder="scheduler")
+    stage2_pipeline = Stage2(stage2_unet,vae,attn_ckpt,stage2_noise_scheduler,args.device)
+    
+    pipe = IMAGGarment(unet=unet, reference_unet=ref_unet, vae=vae, tokenizer=tokenizer,
+                         text_encoder=text_encoder, image_encoder=image_encoder,
+                         ip_ckpt=args.ip_ckpt,
+                         scheduler=noise_scheduler,
+                         stage2=stage2_pipeline,
+                         safety_checker=StableDiffusionSafetyChecker,
+                         feature_extractor=CLIPImageProcessor)
+    return pipe, generator
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='IMAGGarment')
+    parser.add_argument('--stage1_model_ckpt',type=str)
+    parser.add_argument('--stage2_model_ckpt',type=str)
+    parser.add_argument('--prompt',type=str,default="A cloth")
+    parser.add_argument('--sketch_path', type=str, required=True)
+    parser.add_argument('--logo_path', type=str, required=True)
+    parser.add_argument('--mask_path', type=str, required=True)
+    parser.add_argument('--color_path',type=str,required=True)
+    parser.add_argument('--output_path', type=str, default="./output_sd_base")
+    parser.add_argument('--ip_ckpt', type=str, required=True)
+
+    parser.add_argument('--device', type=str, default="cuda:0")
+    parser.add_argument(
+        "--width",
+        type=int,
+        default=512,
+        help=(
+            "The resolution for input images, all the images in the train/validation dataset will be resized to this"
+            " resolution"
+        ),
+    )
+    parser.add_argument(
+        "--height",
+        type=int,
+        default=640,
+        help=(
+            "The resolution for input images, all the images in the train/validation dataset will be resized to this"
+            " resolution"
+        ),
+    )
+    args = parser.parse_args()
+
+    # save path
+    output_path = args.output_path
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    pipe, generator = prepare(args)
+    print('====================== pipe load finish ===================')
+
+    num_samples = 1
+    clip_image_processor = CLIPImageProcessor()
+
+    img_transform = transforms.Compose([
+        transforms.Resize([640, 512], interpolation=transforms.InterpolationMode.BILINEAR),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5]),
+    ])
+
+    
+    #单图片
+    prompt = args.prompt
+    null_prompt = ''
+    negative_prompt = ' worst quality, low quality'
+
+    
+    
+    sketch_img = Image.open(args.sketch_path).convert("RGB")
+    sketch_img = resize_img(sketch_img)
+    vae_sketch = img_transform(sketch_img).unsqueeze(0)
+    logo ,mask = preprocess(Image.open(args.logo_path),Image.open(args.mask_path),args.height,args.width)
+    logo,mask = back_to_pic(logo,mask)
+    
+    if args.color_path is not None:
+        color_image = Image.open(args.color_path)
+    else:
+        color_embeds = None
+        color_clip_image = None
+    
+    output = pipe(
+        ref_image=vae_sketch,
+        logo=logo,
+        mask=mask,
+        prompt=prompt,
+        color_clip_image=color_image,
+        color_embeds=None,
+        null_prompt=null_prompt,
+        negative_prompt=negative_prompt,
+        width=512,
+        height=640,
+        num_images_per_prompt=num_samples,
+        guidance_scale=2.5,
+        sketch_scale=0.7,
+        ipa_scale=0.0,
+        generator=generator,
+        num_inference_steps=50,
+
+    )
+
+    save_output = []
+    save_output.append(output[0])
+    save_output.insert(0,color_image.resize((512, 640), Image.BICUBIC))
+    save_output.insert(0, sketch_img.resize((512, 640), Image.BICUBIC))
+    
+    
+    save_output.insert(1, Image.open(args.logo_path).convert('RGB').resize((512,640),Image.BICUBIC))
+    save_output.insert(1, Image.open(args.mask_path).resize((512,640),Image.BICUBIC))
+    grid = image_grid(save_output, 1, 5)
+    grid.save(
+        output_path + '/' + args.sketch_path.split("/")[-1])
+    
+    print( output_path + '/' + args.sketch_path.split("/")[-1])
---
+++ code/pipelines/stage2_pipeline.py
@@ -0,0 +1,346 @@
+import os
+from typing import List
+import tqdm
+import torch
+from diffusers import StableDiffusionPipeline
+from diffusers.pipelines.controlnet import MultiControlNetModel
+from PIL import Image
+from safetensors import safe_open
+from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
+from accelerate import load_checkpoint_in_model
+from utils import (compute_vae_encodings, numpy_to_pil, prepare_image,
+                   prepare_mask_image, resize_and_crop, resize_and_padding)
+from .utils import is_torch2_available, get_generator
+from diffusers.utils.torch_utils import randn_tensor
+from diffusers.loaders import LoraLoaderMixin
+from diffusers.utils import is_accelerate_available
+from diffusers.schedulers import (
+    DDIMScheduler,
+    DPMSolverMultistepScheduler,
+    EulerAncestralDiscreteScheduler,
+    EulerDiscreteScheduler,
+    LMSDiscreteScheduler,
+    PNDMScheduler,
+)
+from diffusers.pipelines.controlnet.pipeline_controlnet import *
+from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import *
+from typing import Callable, Optional, Union
+logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
+
+
+from adapter.attention_processor import SkipAttnProcessor,SAttnProcessor2_0 as AttnProcessor
+
+
+from .resampler import Resampler
+
+
+class Stage2:
+    def __init__(self, unet,vae, attn_ckpt,noise_scheduler,device,weight_dtype = torch.float16):
+        self.device = device
+        self.unet = unet.to(self.device)
+        self.vae = vae
+        self.attn_ckpt = attn_ckpt
+        self.noise_scheduler = noise_scheduler
+        self.weight_dtype = weight_dtype
+        self.set_attn()
+        self.load_attn()
+
+    def set_attn(self):
+        unet = self.unet
+        attn_procs = {}
+        for name in unet.attn_processors.keys():
+            cross_attention_dim = None if name.endswith("attn1.processor") else unet.config.cross_attention_dim
+            if name.startswith("mid_block"):
+                hidden_size = unet.config.block_out_channels[-1]
+            elif name.startswith("up_blocks"):
+                block_id = int(name[len("up_blocks.")])
+                hidden_size = list(reversed(unet.config.block_out_channels))[block_id]
+            elif name.startswith("down_blocks"):
+                block_id = int(name[len("down_blocks.")])
+                hidden_size = unet.config.block_out_channels[block_id]
+            if cross_attention_dim is None:
+                attn_procs[name] = AttnProcessor(name=name,hidden_size=hidden_size,
+                    cross_attention_dim=cross_attention_dim).to(self.device, dtype=torch.float16)
+            else:
+                attn_procs[name] = SkipAttnProcessor(
+                    name =name,
+                    hidden_size=hidden_size,
+                    cross_attention_dim=cross_attention_dim
+                ).to(self.device, dtype=torch.float16)
+        unet.set_attn_processor(attn_procs)
+
+    def load_attn(self):
+        
+        attn_blocks = torch.nn.ModuleList()
+        for name, param in self.unet.named_modules():
+            if "attn1" in name:
+                attn_blocks.append(param)
+        sd_state = torch.load(self.attn_ckpt, map_location="cpu",weights_only=True)
+        attn_sd = {}
+        for k in sd_state:
+            if 'attn_block' in k:
+                attn_sd[k.replace('attn_block.','')] = sd_state[k]
+        attn_blocks.load_state_dict(attn_sd)
+        # load_checkpoint_in_model(attn_blocks,self.attn_ckpt)
+    def check_inputs(self, image, condition_image, mask, width, height):
+        if isinstance(image, torch.Tensor) and isinstance(condition_image, torch.Tensor) and isinstance(mask, torch.Tensor):
+            return image, condition_image, mask
+        assert image.size == mask.size, "Image and mask must have the same size"
+        image = resize_and_crop(image, (width, height))
+        mask = resize_and_crop(mask, (width, height))
+        condition_image = resize_and_padding(condition_image, (width, height))
+        return image, condition_image, mask
+    def encode_prompt(
+            self,
+            prompt,
+            device,
+            num_images_per_prompt,
+            do_classifier_free_guidance,
+            negative_prompt=None,
+            prompt_embeds: Optional[torch.FloatTensor] = None,
+            negative_prompt_embeds: Optional[torch.FloatTensor] = None,
+            lora_scale: Optional[float] = None,
+            clip_skip: Optional[int] = None,
+    ):
+
+        if lora_scale is not None and isinstance(self, LoraLoaderMixin):
+            self._lora_scale = lora_scale
+
+            # dynamically adjust the LoRA scale
+            if not USE_PEFT_BACKEND:
+                adjust_lora_scale_text_encoder(self.text_encoder, lora_scale)
+            else:
+                scale_lora_layers(self.text_encoder, lora_scale)
+
+        if prompt is not None and isinstance(prompt, str):
+            batch_size = 1
+        elif prompt is not None and isinstance(prompt, list):
+            batch_size = len(prompt)
+        else:
+            batch_size = prompt_embeds.shape[0]
+
+        if prompt_embeds is None:
+            # textual inversion: procecss multi-vector tokens if necessary
+            if isinstance(self, TextualInversionLoaderMixin):
+                prompt = self.maybe_convert_prompt(prompt, self.tokenizer)
+
+            text_inputs = self.tokenizer(
+                prompt,
+                padding="max_length",
+                max_length=self.tokenizer.model_max_length,
+                truncation=True,
+                return_tensors="pt",
+            )
+            text_input_ids = text_inputs.input_ids
+            untruncated_ids = self.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
+
+            if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(
+                    text_input_ids, untruncated_ids
+            ):
+                removed_text = self.tokenizer.batch_decode(
+                    untruncated_ids[:, self.tokenizer.model_max_length - 1: -1]
+                )
+                logger.warning(
+                    "The following part of your input was truncated because CLIP can only handle sequences up to"
+                    f" {self.tokenizer.model_max_length} tokens: {removed_text}"
+                )
+
+            if hasattr(self.text_encoder.config, "use_attention_mask") and self.text_encoder.config.use_attention_mask:
+                attention_mask = text_inputs.attention_mask.to(device)
+            else:
+                attention_mask = None
+
+            if clip_skip is None:
+                prompt_embeds = self.text_encoder(text_input_ids.to(device), attention_mask=attention_mask)
+                prompt_embeds = prompt_embeds[0]
+            else:
+                prompt_embeds = self.text_encoder(
+                    text_input_ids.to(device), attention_mask=attention_mask, output_hidden_states=True
+                )
+                # Access the `hidden_states` first, that contains a tuple of
+                # all the hidden states from the encoder layers. Then index into
+                # the tuple to access the hidden states from the desired layer.
+                prompt_embeds = prompt_embeds[-1][-(clip_skip + 1)]
+                # We also need to apply the final LayerNorm here to not mess with the
+                # representations. The `last_hidden_states` that we typically use for
+                # obtaining the final prompt representations passes through the LayerNorm
+                # layer.
+                prompt_embeds = self.text_encoder.text_model.final_layer_norm(prompt_embeds)
+
+        if self.text_encoder is not None:
+            prompt_embeds_dtype = self.text_encoder.dtype
+        elif self.unet is not None:
+            prompt_embeds_dtype = self.unet.dtype
+        else:
+            prompt_embeds_dtype = prompt_embeds.dtype
+
+        prompt_embeds = prompt_embeds.to(dtype=prompt_embeds_dtype, device=device)
+
+        bs_embed, seq_len, _ = prompt_embeds.shape
+        # duplicate text embeddings for each generation per prompt, using mps friendly method
+        prompt_embeds = prompt_embeds.repeat(1, num_images_per_prompt, 1)
+        prompt_embeds = prompt_embeds.view(bs_embed * num_images_per_prompt, seq_len, -1)
+
+        # get unconditional embeddings for classifier free guidance
+        if do_classifier_free_guidance and negative_prompt_embeds is None:
+            uncond_tokens: List[str]
+            if negative_prompt is None:
+                uncond_tokens = [""] * batch_size
+            elif prompt is not None and type(prompt) is not type(negative_prompt):
+                raise TypeError(
+                    f"`negative_prompt` should be the same type to `prompt`, but got {type(negative_prompt)} !="
+                    f" {type(prompt)}."
+                )
+            elif isinstance(negative_prompt, str):
+                uncond_tokens = [negative_prompt]
+            elif batch_size != len(negative_prompt):
+                raise ValueError(
+                    f"`negative_prompt`: {negative_prompt} has batch size {len(negative_prompt)}, but `prompt`:"
+                    f" {prompt} has batch size {batch_size}. Please make sure that passed `negative_prompt` matches"
+                    " the batch size of `prompt`."
+                )
+            else:
+                uncond_tokens = negative_prompt
+
+            # textual inversion: procecss multi-vector tokens if necessary
+            if isinstance(self, TextualInversionLoaderMixin):
+                uncond_tokens = self.maybe_convert_prompt(uncond_tokens, self.tokenizer)
+
+            max_length = prompt_embeds.shape[1]
+            uncond_input = self.tokenizer(
+                uncond_tokens,
+                padding="max_length",
+                max_length=max_length,
+                truncation=True,
+                return_tensors="pt",
+            )
+
+            if hasattr(self.text_encoder.config, "use_attention_mask") and self.text_encoder.config.use_attention_mask:
+                attention_mask = uncond_input.attention_mask.to(device)
+            else:
+                attention_mask = None
+
+            negative_prompt_embeds = self.text_encoder(
+                uncond_input.input_ids.to(device),
+                attention_mask=attention_mask,
+            )
+            negative_prompt_embeds = negative_prompt_embeds[0]
+
+        if do_classifier_free_guidance:
+            # duplicate unconditional embeddings for each generation per prompt, using mps friendly method
+            seq_len = negative_prompt_embeds.shape[1]
+
+            negative_prompt_embeds = negative_prompt_embeds.to(dtype=prompt_embeds_dtype, device=device)
+
+            negative_prompt_embeds = negative_prompt_embeds.repeat(1, num_images_per_prompt, 1)
+            negative_prompt_embeds = negative_prompt_embeds.view(batch_size * num_images_per_prompt, seq_len, -1)
+
+        if isinstance(self, LoraLoaderMixin) and USE_PEFT_BACKEND:
+            # Retrieve the original scale by scaling back the LoRA layers
+            unscale_lora_layers(self.text_encoder, lora_scale)
+
+        return prompt_embeds, negative_prompt_embeds
+    
+    
+    @torch.no_grad()
+    def generate(
+        self,
+        image,
+        condition_image,
+        mask,
+        num_inference_steps: int = 50,
+        guidance_scale: float = 2.5,
+        height: int = 640,
+        width: int = 512,
+        generator=None,
+        eta=1.0,
+        **kwargs,
+    ):
+        concat_dim = -2  # FIXME: y axis concat
+        # Prepare inputs to Tensor
+        condition_image = prepare_image(condition_image).to(self.device, dtype=self.weight_dtype)
+        mask = prepare_mask_image(mask).to(self.device, dtype=self.weight_dtype)
+        
+
+        # VAE encoding
+        condition_latent = compute_vae_encodings(condition_image, self.vae).to(self.device)
+        mask_latent = torch.nn.functional.interpolate(mask, size=condition_latent.shape[-2:], mode="nearest").to(self.device)
+        masked_latent = image * (mask_latent < 0.5)
+        del image, mask, condition_image
+        # Concatenate latents
+        masked_latent_concat = torch.cat([masked_latent, condition_latent], dim=concat_dim).to(self.device)
+        mask_latent_concat = torch.cat([mask_latent, torch.zeros_like(mask_latent)], dim=concat_dim).to(self.device)
+        
+        # Sample noise that we'll add to the latents
+        latents = randn_tensor(
+            masked_latent_concat.shape,
+            generator=generator,
+            device=masked_latent_concat.device,
+            dtype=self.weight_dtype,
+        )
+        # Prepare timesteps
+        self.noise_scheduler.set_timesteps(num_inference_steps, device=self.device)
+        timesteps = self.noise_scheduler.timesteps
+        latents = latents * self.noise_scheduler.init_noise_sigma
+
+       
+        # Classifier-Free Guidance
+        if do_classifier_free_guidance := (guidance_scale > 1.0):
+            masked_latent_concat = torch.cat(
+                [
+                    torch.cat([masked_latent, torch.zeros_like(condition_latent)], dim=concat_dim),
+                    masked_latent_concat,
+                ]
+            )
+            mask_latent_concat = torch.cat([mask_latent_concat] * 2)
+
+        # Denoising loop
+        num_warmup_steps = (len(timesteps) - num_inference_steps * self.noise_scheduler.order)
+        with tqdm.tqdm(total=num_inference_steps) as progress_bar:
+            for i, t in enumerate(timesteps):
+                # expand the latents if we are doing classifier free guidance
+                
+                non_inpainting_latent_model_input = (torch.cat([latents] * 2) if do_classifier_free_guidance else latents)
+                non_inpainting_latent_model_input = self.noise_scheduler.scale_model_input(non_inpainting_latent_model_input, t)
+                # prepare the input for the inpainting model
+                # concat in channel
+                inpainting_latent_model_input = torch.cat([non_inpainting_latent_model_input, mask_latent_concat, masked_latent_concat], dim=1)
+                # predict the noise residual
+                noise_pred= self.unet(
+                    inpainting_latent_model_input,
+                    t.to(self.device),
+                    encoder_hidden_states=None, # FIXME
+                    return_dict=False,
+                )[0]
+                # perform guidance
+                if do_classifier_free_guidance:
+                    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                    noise_pred = noise_pred_uncond + guidance_scale * (
+                        noise_pred_text - noise_pred_uncond
+                    )
+                # compute the previous noisy sample x_t -> x_t-1
+                latents = self.noise_scheduler.step(
+                    noise_pred, t, latents, **kwargs
+                ).prev_sample
+                # call the callback, if provided
+                if i == len(timesteps) - 1 or (
+                    (i + 1) > num_warmup_steps
+                    and (i + 1) % self.noise_scheduler.order == 0
+                ):
+                    progress_bar.update()
+                    
+
+        # Decode the final latents
+        latents = latents.split(latents.shape[concat_dim] // 2, dim=concat_dim)[0]
+        latents = 1 / self.vae.config.scaling_factor * latents
+        image = self.vae.decode(latents.to(self.device, dtype=self.weight_dtype)).sample
+        image = (image / 2 + 0.5).clamp(0, 1)
+        # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
+        image = image.cpu().permute(0, 2, 3, 1).float().detach().numpy()
+        image = numpy_to_pil(image)
+        
+        
+        return image
+
+
+
